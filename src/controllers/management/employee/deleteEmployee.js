3068e688a72589ab7d7bc1f451b2ac48c72f69f9
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-05 20:02:42 +0100

    feat: add feature to delete employee

---
+++ src/controllers/management/employee/deleteEmployee.js
@@ -0,0 +1,39 @@
+import { InvalidEntry } from '../../errors/invalidEntry'
+import { ServerError } from '../../errors/serverError'
+
+export class DeleteEmployeeController {
+  constructor (
+    Connection,
+    EmployeeRepository
+  ) {
+    this.conn = new Connection()
+    this.employeeRepository = new EmployeeRepository(this.conn)
+  }
+
+  async handle (req) {
+    const id = req.body?.id
+
+    try {
+      await this.employeeRepository.delete(id)
+
+      if ( !id ) {
+        return {
+          code: 400,
+          error: new InvalidEntry('id')
+        }
+      }
+
+      this.conn.close()
+    } catch (error) {
+      return {
+        code: 500,
+        error: new ServerError()
+      }
+    }
+
+    return {
+      code: 204,
+      success: true
+    }
+  }
+}
