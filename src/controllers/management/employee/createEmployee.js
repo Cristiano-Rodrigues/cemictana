6a5a91bd04f53fd71f5cea3de7c6fc5cde444374
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-05 18:18:14 +0100

    feat: should return an error object if any internal server error

--- src/controllers/management/employee/createEmployee.js
+++ src/controllers/management/employee/createEmployee.js
@@ -1,5 +1,6 @@
 import { DuplicatedData } from '../../errors/duplicatedData'
 import { InvalidEntry } from '../../errors/invalidEntry'
+import { ServerError } from '../../errors/serverError'
 
 export class CreateEmployeeController {
   constructor (
@@ -29,24 +30,31 @@ export class CreateEmployeeController {
       }
     }
 
-    const alreadyExists = await this.employeeRepository.getByIdentification(identification)
+    try {
+      const alreadyExists = await this.employeeRepository.getByIdentification(identification)
 
-    if ( alreadyExists ) {
+      if ( alreadyExists ) {
+        return {
+          code: 400,
+          error: new DuplicatedData(identification)
+        }
+      }
+
+      await this.employeeRepository.create({
+        name,
+        identification,
+        post,
+        bornDate,
+        address
+      })
+
+      this.conn.close()
+    } catch (error) {
       return {
-        code: 400,
-        error: new DuplicatedData(identification)
+        code: 500,
+        error: new ServerError()
       }
     }
-
-    await this.employeeRepository.create({
-      name,
-      identification,
-      post,
-      bornDate,
-      address
-    })
-
-    this.conn.close()
 
     return {
       code: 201,
