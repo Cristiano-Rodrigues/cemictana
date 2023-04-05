c2066415c06726b065d847e92642d9c7fe03e785
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-05 18:31:59 +0100

    feat: add feature to get all registered employees

---
+++ src/controllers/management/employee/getEmployees.js
@@ -0,0 +1,30 @@
+import { ServerError } from '../../errors/serverError'
+
+export class GetEmployeesController {
+  constructor (
+    Connection,
+    EmployeeRepository
+  ) {
+    this.conn = new Connection()
+    this.employeeRepository = new EmployeeRepository(this.conn)
+  }
+
+  async handle (_) {
+    try {
+      const result = await this.employeeRepository.get()
+
+      this.conn.close()
+
+      return {
+        code: 200,
+        success: true,
+        result
+      }
+    } catch (error) {
+      return {
+        code: 500,
+        error: new ServerError()
+      }
+    }
+  }
+}
