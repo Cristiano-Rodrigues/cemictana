2b739b5a0110bd7694989e8fd067eba745408310
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-05 21:58:33 +0100

    feat: add routes to manage employees

---
+++ src/routes/employee.js
@@ -0,0 +1,27 @@
+import {
+  CreateEmployeeController,
+  GetEmployeesController,
+  UpdateEmployeeController,
+  DeleteEmployeeController
+} from '../controllers/management/employee'
+import { Connection, EmployeeRepository } from '../repositories'
+import { adaptController } from './adapters/express-controller-adapter'
+
+export default router => {
+  const params = [
+    Connection,
+    EmployeeRepository
+  ]
+  
+  const createEmployeeController = new CreateEmployeeController(...params)
+  const getEmployeesController = new GetEmployeesController(...params)
+  const updateEmployeeController = new UpdateEmployeeController(...params)
+  const deleteEmployeeController = new DeleteEmployeeController(...params)
+
+  router.post('/employee', adaptController(createEmployeeController))
+  router.get('/employee', adaptController(getEmployeesController))
+  router.put('/employee', adaptController(updateEmployeeController))
+  router.delete('/employee', adaptController(deleteEmployeeController))
+
+  return router
+}
