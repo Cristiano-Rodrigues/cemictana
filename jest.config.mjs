cf8a22b805de1f07ebd895c1e9564fc10f247177
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-05 11:45:05 +0100

    chore: add jest

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  clearMocks: true,
+
+  collectCoverageFrom: [
+    '<rootDir>/controllers/**/*.js'
+  ],
+
+  coverageDirectory: 'coverage',
+
+  coverageProvider: 'v8',
+
+  rootDir: './src',
+
+};
