605ae60b02b1893350d53fbf320914a575276892
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-05 10:44:59 +0100

    feat: add employee repository

---
+++ src/repositories/employee.js
@@ -0,0 +1,58 @@
+export class EmployeeRepository {
+  constructor (connection) {
+    this.query = connection.query
+  }
+
+  async create ({
+    name, identification, post, bornDate, address
+  }) {
+    const sql = {
+      query: 'INSERT INTO employee VALUES (default, ?, ?, ?, ?, ?); \
+        \ SELECT LAST_INSERT_ID();',
+      values: [name, identification, post, bornDate, address]
+    }
+    await this.query(sql.query, sql.values)
+  }
+
+  async get () {
+    const sql = {
+      query: 'SELECT * FROM employee ORDER BY `name` LIMIT 1000'
+    }
+    return await this.query(sql.query)
+  }
+
+  async getByName (name) {
+    const sql = {
+      query: 'SELECT * FROM employee WHERE name=? LIMIT 1',
+      values: [name]
+    }
+    return (await this.query(sql.query, sql.values))[0]
+  }
+
+  async getById (id) {
+    const sql = {
+      query: 'SELECT * FROM employee WHERE id=? LIMIT 1',
+      values: [id]
+    }
+    return (await this.query(sql.query, sql.values))[0]
+  }
+
+  async update (id, {
+    name, identification, post, bornDate, address
+  }) {
+    const sql = {
+      query: 'UPDATE employee SET name=?, identification=?, post=?, \
+        \ bornDate=?, address=? LIMIT 1',
+      values: [name, identification, post, bornDate, address, id]
+    }
+    await this.query(sql.query, sql.values)
+  }
+
+  async delete (id) {
+    const sql = {
+      query: 'DELETE FROM employee WHERE id=? LIMIT 1',
+      values: [id]
+    }
+    await this.query(sql.query, sql.values)
+  }
+}
