a91f0efba8c9a90554bd0e9f2f0910d8749df8ee
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-06 13:33:23 +0100

    feat: add feature to delete units

---
+++ src/controllers/management/unit/deleteUnit.js
@@ -0,0 +1,38 @@
+import { InvalidEntry, ServerError } from '../../errors/'
+
+export class DeleteUnitController {
+  constructor (
+    Connection,
+    UnitRepository
+  ) {
+    this.conn = new Connection()
+    this.unitRepository = new UnitRepository(this.conn)
+  }
+
+  async handle (req) {
+    const id = req.body?.id
+
+    if ( !id ) {
+      return {
+        code: 400,
+        error: new InvalidEntry('id')
+      }
+    }
+    
+    try {
+      await this.unitRepository.delete(id)
+
+      this.conn.close()
+    } catch (error) {
+      return {
+        code: 500,
+        error: new ServerError()
+      }
+    }
+
+    return {
+      code: 204,
+      success: true
+    }
+  }
+}
