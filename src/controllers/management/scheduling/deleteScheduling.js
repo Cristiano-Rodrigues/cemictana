f5fb76fced8ba7114d377a34c1ce6bde3f384513
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-07 16:07:16 +0100

    feat: add feature to delete scheduling

---
+++ src/controllers/management/scheduling/deleteScheduling.js
@@ -0,0 +1,41 @@
+import { InvalidEntry, ServerError } from '../../errors/'
+
+export class DeleteSchedulingController {
+  constructor (
+    Connection,
+    SchedulingRepository
+  ) {
+    this.Connection = Connection
+    this.SchedulingRepository = SchedulingRepository
+  }
+
+  async handle (req) {
+    const id = req.params?.id
+
+    if ( !id ) {
+      return {
+        code: 400,
+        error: new InvalidEntry('id')
+      }
+    }
+
+    try {
+      const conn = new this.Connection()
+      const schedulingRepository = new this.SchedulingRepository(conn)
+      
+      await schedulingRepository.delete(id)
+
+      conn.close()
+    } catch (error) {
+      return {
+        code: 500,
+        error: new ServerError()
+      }
+    }
+
+    return {
+      code: 204,
+      success: true
+    }
+  }
+}
