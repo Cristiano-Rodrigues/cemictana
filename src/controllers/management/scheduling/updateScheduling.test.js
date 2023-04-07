16844ea2c437fde41cb2cb2d4c2c97f094982f27
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-07 16:02:35 +0100

    feat: add feature to update scheduling

---
+++ src/controllers/management/scheduling/updateScheduling.test.js
@@ -0,0 +1,160 @@
+import { InvalidEntry , ServerError } from '../../errors/'
+import { UpdateSchedulingController } from './updateScheduling'
+
+const ConnectionStub = class {
+  close () {}
+}
+const SchedulingRepositoryStub = class {
+  async update () {}
+}
+const ResponsibleRepositoryStub = class {
+  async getById (id) {
+    return { id }
+  }
+}
+const DefunctRepositoryStub = class {
+  async getById (id) {
+    return { id }
+  }
+}
+const UnitRepositoryStub = class {
+  async getById (id) {
+    return { id }
+  }
+}
+const EmployeeRepositoryStub = class {
+  async getById (id) {
+    return { id }
+  }
+}
+
+const updateSchedulingController = new UpdateSchedulingController(
+  ConnectionStub,
+  SchedulingRepositoryStub,
+  ResponsibleRepositoryStub,
+  DefunctRepositoryStub,
+  UnitRepositoryStub,
+  EmployeeRepositoryStub
+)
+
+const tomorrow = () => {
+  const d = new Date()
+  const datestring = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate() + 1}`
+  return datestring
+}
+
+describe('UpdateSchedulingController', () => {
+  test('Should return an error object if null is given for any required value', async () => {
+    const result = await updateSchedulingController.handle({
+      body: {
+        id: null,
+        type: 'any_type',
+        schedulingDate: '2023-04-06',
+        responsible: 1,
+        defunct: 1,
+        employee: 1,
+        unit: 1
+      }
+    })
+    expect(result.code).toBe(400)
+    expect((result.error instanceof InvalidEntry)).toBe(true)
+  })
+
+  test('Should return an error object if given schedulingDate is before current date', async () => {
+    const result = await updateSchedulingController.handle({
+      body: {
+        id: 1,
+        type: 'any_type',
+        schedulingDate: '2023-04-06',
+        responsible: 1,
+        defunct: 1,
+        employee: 1,
+        unit: 1
+      }
+    })
+    expect(result).toEqual({
+      code: 400,
+      error: new InvalidEntry('schedulingDate')
+    })
+  })
+
+  test('Should return an error object if invalid id is given for responsible, defunct, employee or unit', async () => {
+    const ResponsibleRepositoryStub = class {
+      getById (_) {
+        return null
+      }
+    }
+    const updateSchedulingController = new UpdateSchedulingController(
+      ConnectionStub,
+      SchedulingRepositoryStub,
+      ResponsibleRepositoryStub,
+      DefunctRepositoryStub,
+      UnitRepositoryStub,
+      EmployeeRepositoryStub
+    )
+
+    const result = await updateSchedulingController.handle({
+      body: {
+        id: 1,
+        type: 'any_type',
+        schedulingDate: tomorrow(),
+        responsible: 'non-existing-id',
+        defunct: 1,
+        employee: 1,
+        unit: 1
+      }
+    })
+    expect(result.code).toBe(400)
+    expect((result.error instanceof InvalidEntry)).toBe(true)
+  })
+
+  test('Should return an error object if any internal server error', async () => {
+    const ResponsibleRepositoryStub = class {
+      getById (_) {
+        throw new Error('any_error')
+      }
+    }
+    const updateSchedulingController = new UpdateSchedulingController(
+      ConnectionStub,
+      SchedulingRepositoryStub,
+      ResponsibleRepositoryStub,
+      DefunctRepositoryStub,
+      UnitRepositoryStub,
+      EmployeeRepositoryStub
+    )
+
+    const result = await updateSchedulingController.handle({
+      body: {
+        id: 1,
+        type: 'any_type',
+        schedulingDate: tomorrow(),
+        responsible: 1,
+        defunct: 1,
+        employee: 1,
+        unit: 1
+      }
+    })
+    expect(result).toEqual({
+      code: 500,
+      error: new ServerError()
+    })
+  })
+
+  test('Should return a success object if no error', async () => {
+    const result = await updateSchedulingController.handle({
+      body: {
+        id: 1,
+        type: 'any_type',
+        schedulingDate: tomorrow(),
+        responsible: 1,
+        defunct: 1,
+        employee: 1,
+        unit: 1
+      }
+    })
+    expect(result).toEqual({
+      code: 201,
+      success: true
+    })
+  })
+})
