75acf77ed81da58b5001fc32f1fe03594b3395a4
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-07 15:44:35 +0100

    fix: remove unnecessary columns

--- src/repositories/scheduling.js
+++ src/repositories/scheduling.js
@@ -4,12 +4,12 @@ export class SchedulingRepository {
   }
 
   async create ({
-    type, schedulingDate, occurenceDate, responsible, defunct, employee, unit
+    type, schedulingDate, responsible, defunct, employee, unit
   }) {
     const sql = {
-      query: 'INSERT INTO scheduling VALUES (default, ?, ?, ?, ?, ?, ?, ?); \
+      query: 'INSERT INTO scheduling VALUES (default, ?, ?, ?, ?, ?, ?); \
         \ SELECT LAST_INSERT_ID();',
-      values: [type, schedulingDate, occurenceDate, responsible, defunct, employee, unit]
+      values: [type, schedulingDate, responsible, defunct, employee, unit]
     }
     await this.query(sql.query, sql.values)
   }
@@ -30,12 +30,12 @@ export class SchedulingRepository {
   }
 
   async update (id, {
-    type, schedulingDate, occurenceDate, responsible, defunct, employee, unit
+    type, schedulingDate, responsible, defunct, employee, unit
   }) {
     const sql = {
-      query: 'UPDATE scheduling SET type=?, schedulingDate=?, occurenceDate=?, \
-        \ responsible=?, defunct=?, employee=?, unit=? WHERE id=? LIMIT 1',
-      values: [type, schedulingDate, occurenceDate, responsible, defunct, employee, unit, id]
+      query: 'UPDATE scheduling SET type=?, schedulingDate=?, responsible=?, \
+        \ defunct=?, employee=?, unit=? WHERE id=? LIMIT 1',
+      values: [type, schedulingDate, responsible, defunct, employee, unit, id]
     }
     await this.query(sql.query, sql.values)
   }
