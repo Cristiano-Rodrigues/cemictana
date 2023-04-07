05858506bc65b25e02f845b9e23d900f8ec63680
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-04-07 16:07:34 +0100

    test: add feature to delete scheduling

---
+++ src/controllers/management/scheduling/deleteScheduling.test.js
@@ -0,0 +1,59 @@
+import { InvalidEntry, ServerError } from '../../errors/'
+import { DeleteSchedulingController } from './deleteScheduling'
+
+const ConnectionStub = class {
+  close () {}
+}
+const SchedulingRepositoryStub = class {
+  async delete () {}
+}
+
+const deleteSchedulingController = new DeleteSchedulingController(
+  ConnectionStub,
+  SchedulingRepositoryStub
+)
+
+describe('DeleteSchedulingController', () => {
+  test('Should return an error object if no id is provided', async () => {
+    const result = await deleteSchedulingController.handle({})
+    expect(result).toEqual({
+      code: 400,
+      error: new InvalidEntry('id')
+    })
+  })
+
+  test('Should return an error object if any internal server error', async () => {
+    const SchedulingRepositoryStub = class {
+      delete () {
+        throw new Error('any_error')
+      }
+    }
+    const deleteSchedulingController = new DeleteSchedulingController(
+      ConnectionStub,
+      SchedulingRepositoryStub
+    )
+
+    const result = await deleteSchedulingController.handle({
+      params: {
+        id: 1
+      }
+    })
+    
+    expect(result).toEqual({
+      code: 500,
+      error: new ServerError()
+    })
+  })
+
+  test('Should return a success object if no error', async () => {
+    const result = await deleteSchedulingController.handle({
+      params: {
+        id: 1
+      }
+    })
+    expect(result).toEqual({
+      code: 204,
+      success: true
+    })
+  })
+})
