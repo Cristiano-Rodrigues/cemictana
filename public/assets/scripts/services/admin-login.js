603340f11400d7c608927e0da04a82659d7eaff3
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-05-09 07:11:50 +0100

    feat: add feature for admin login

---
+++ public/assets/scripts/services/admin-login.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('form')
+const alert = document.getElementById('alert')
+const message = alert.querySelector('.message')
+
+form.addEventListener('submit', async evt => {
+  evt.preventDefault()
+
+  try {
+    const response = await request({
+      url: 'http://localhost:8080/api/v1/login',
+      method: 'POST',
+      body: JSON.stringify({
+        email: form.elements.email.value,
+        password: form.elements.password.value
+      })
+    })
+    if (response.code != 200 || !response.success) {
+      message.innerText = mapError(response.error.name)
+      alert.classList.add('visible')
+      return
+    }
+    localStorage.setItem('token', response.result.token)
+    window.location.href = './admin/'
+  } catch (error) {
+    console.error(error)
+  }
+  
+})
