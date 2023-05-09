88f4d0e2bf7fe315a769eefca3f578366ec816f1
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-05-09 11:58:05 +0100

    feat: add session store

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
 import { Unauthorized } from './src/middlewares/errors/unauthorized'
+import expressMySqlSession from 'express-mysql-session'
 
 dotenv.config()
 
@@ -11,9 +12,19 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+const MySQLStore = expressMySqlSession(session)
+const sessionStore = new MySQLStore({
+  host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME
+})
+
 app.use(session({
   secret: process.env.SECRET,
   resave: false,
+  store: sessionStore,
   saveUninitialized: false,
   cookie: {
     maxAge: 24 * 1000 * 60 * 60,
