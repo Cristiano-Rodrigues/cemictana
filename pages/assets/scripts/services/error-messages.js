d732462073b0db39d79e86adc4f9373749ed2751
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-05-11 11:27:34 +0100

    feat: map server error message

--- pages/assets/scripts/services/error-messages.js
+++ pages/assets/scripts/services/error-messages.js
@@ -1,7 +1,8 @@
 const map = {
   Unauthorized: 'Credenciais incorrectas. Tente novamente',
   InvalidEntry: 'Insira correctamente os dados requisitados',
-  DuplicatedData: 'Nome ou identificação já existem'
+  DuplicatedData: 'Nome ou identificação já existem',
+  ServerError: 'Algo correu mal. Tente novamente mais tarde'
 }
 
 function mapError(error) {
