04ff15c8a692bab9a330ce12ff338e15f6d7a728
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-05-11 12:25:03 +0100

    test: add feature to search defuncts by name

---
+++ src/controllers/management/defunct/searchDefunctName.test.js
@@ -0,0 +1,54 @@
+import { ServerError } from '../../errors'
+import { SearchDefunctNameController } from './searchDefunctName'
+
+const ConnectionStub = class {
+  close () {}
+}
+const DefunctRepositoryStub = class {
+  async searchByName () {
+    return []
+  }
+}
+
+const searchDefunctNameController = new SearchDefunctNameController(
+  ConnectionStub,
+  DefunctRepositoryStub
+)
+
+describe('SearchDefunctNameController', () => {
+  test('Should return an error object if any internal server error', async () => {
+    const DefunctRepositoryStub = class {
+      searchByName () {
+        throw new Error('any_error')
+      }
+    }
+    const searchDefunctNameController = new SearchDefunctNameController(
+      ConnectionStub,
+      DefunctRepositoryStub
+    )
+
+    const result = await searchDefunctNameController.handle({
+      params: {
+        search: 'any_search'
+      }
+    })
+    expect(result).toEqual({
+      code: 500,
+      error: new ServerError()
+    })
+  })
+
+  test('Should return a success object if no error', async () => {
+    const result = await searchDefunctNameController.handle({
+      params: {
+        search: 'any_search'
+      }
+    })
+
+    expect(result).toEqual({
+      code: 200,
+      success: true,
+      result: []
+    })
+  })
+})
