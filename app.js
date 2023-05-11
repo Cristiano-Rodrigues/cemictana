9107a9903fcbf94099a7a5c916977144ab70f731
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-05-11 11:09:11 +0100

    refactor: fix indentation

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.use(express.urlencoded({ extended: false }))
 const MySQLStore = expressMySqlSession(session)
 const sessionStore = new MySQLStore({
   host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: process.env.DB_NAME
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 })
 
 app.use(session({
