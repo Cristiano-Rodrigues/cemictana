41822928ba00d49fa3237eb68c3a5827cf0fb5cc
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-06-30 09:16:05 +0100

    feat: include state and deleted fields in database

--- src/repositories/scheduling.js
+++ src/repositories/scheduling.js
@@ -7,9 +7,9 @@ export class SchedulingRepository {
     type, schedulingDate, defunct, employee, unit
   }) {
     const sql = {
-      query: 'INSERT INTO scheduling VALUES (default, ?, ?, ?, ?, ?); \
+      query: 'INSERT INTO scheduling VALUES (default, ?, ?, ?, ?, ?, ?, ?); \
         \ SELECT LAST_INSERT_ID();',
-      values: [type, schedulingDate, defunct, employee, unit]
+      values: [type, schedulingDate, defunct, employee, unit, 0, 0]
     }
     await this.query(sql.query, sql.values)
   }
@@ -30,19 +30,19 @@ export class SchedulingRepository {
   }
 
   async update (id, {
-    type, schedulingDate, defunct, employee, unit
+    type, schedulingDate, defunct, employee, unit, state, deleted
   }) {
     const sql = {
       query: 'UPDATE scheduling SET type=?, schedulingDate=?, \
-        \ defunct=?, employee=?, unit=? WHERE id=? LIMIT 1',
-      values: [type, schedulingDate, defunct, employee, unit, id]
+        \ defunct=?, employee=?, unit=?, state=?, deleted=? WHERE id=? LIMIT 1',
+      values: [type, schedulingDate, defunct, employee, unit, state, deleted, id]
     }
     await this.query(sql.query, sql.values)
   }
 
   async delete (id) {
     const sql = {
-      query: 'DELETE FROM scheduling WHERE id=? LIMIT 1',
+      query: 'UPDATE scheduling SET deleted=1 WHERE id=? LIMIT 1',
       values: [id]
     }
     await this.query(sql.query, sql.values)
