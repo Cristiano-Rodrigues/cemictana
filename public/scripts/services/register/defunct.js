116a684be216b6ed3b6d18123ad7284d9a7931ab
Cristiano Rodrigues <realchrisvibez@gmail.com>
2023-07-02 16:56:37 +0100

    feat: add suport to user to send images

--- public/scripts/services/register/defunct.js
+++ public/scripts/services/register/defunct.js
@@ -20,6 +20,12 @@ form.addEventListener('submit', async evt => {
         token
       })
     })
+
+    if (files.length == 2) {
+      const formData = fulfillFormData(files)
+      
+      await requestUploadDefunctImage(response.defunct.id, formData)
+    }
     
     if (response.code != 201 || !response.success) {
       alertDanger.querySelector('.message').innerText = mapError(response.error.name)
@@ -27,6 +33,7 @@ form.addEventListener('submit', async evt => {
       alertDanger.classList.add('visible')
       return
     }
+
     alertDanger.classList.remove('visible')
     alertSuccess.classList.add('visible')
     alertSuccess.querySelector('.message').innerText = 'Defunto cadastrado'
@@ -35,3 +42,40 @@ form.addEventListener('submit', async evt => {
   }
   form.reset()
 })
+
+const { document: documentFile, image } = form.elements
+const files = []
+const onChange = evt => {
+  const fileReader = new FileReader()
+  const file = evt.target.files[0]
+  fileReader.addEventListener('load', async () => {
+    files.push({
+      name: evt.target.name,
+      file
+    })
+  })
+  if (!file) return
+  fileReader.readAsDataURL(file)
+}
+function fulfillFormData (files) {
+  const formData = new FormData()
+    
+  for (const { name, file } of files) {
+    formData.append(name, file)
+  }
+
+  return formData
+}
+async function requestUploadDefunctImage (defunctId, formData) {
+  await fetch(
+    `http://localhost:8080/api/v1/defunct/images?id=${defunctId}`, {
+    headers: {
+      'enctype': 'multipart/form-data'
+    },
+    method: 'PUT',
+    body: formData
+  })
+}
+
+documentFile.addEventListener('change', onChange)
+image.addEventListener('change', onChange)
